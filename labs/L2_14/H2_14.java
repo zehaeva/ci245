ef6f4479908d215ee9c346490b43818a205b28b2
Howard Canaway <howard.canaway@gmail.com>
2016-08-26 09:22:28 -0400

    Lab #1 2.14
    Moved what I thought was a homework problem into the labs directory.

---
+++ labs/L2_14/H2_14.java
@@ -0,0 +1,7 @@
+class H2_14 {
+	public static void main (String[] args) {
+		System.out.println("1 2 3 4");
+		System.out.printf("%s %s %s %s\n", 1, 2, 3, 4);
+		System.out.print("1 2 3 4\n");
+	}
+}
