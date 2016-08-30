5754ebbccbd847330cae339213e4339f8932c054
Howard Canaway <howard.canaway@gmail.com>
2016-08-30 17:22:55 -0400

    Lab 2.17
    Inital Commit, it does most everything except for the largest and
    smallest outputs.

---
+++ labs/L2_17/L2_17.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class L2_17 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int first;
+		int second;
+		int third;
+
+		char cont = 'y';
+
+		try {
+			while (conto == 'y') {
+				System.out.print("Please enter an integer: ");
+				first = input.nextInt();
+				System.out.print("Please enter another integer: ");
+				second = input.nextInt();
+				System.out.print("Please enter one last integer: ");
+				third = input.nextInt();
+
+				System.out.printf("Sum: %d\n", first + second + third);
+				System.out.printf("Average: %d\n", (first + second + third) / 3);
+				System.out.printf("Product: %d\n", first * second * third);
+
+				System.out.printf("Product: %d\n", first * second * third);
+
+				System.out.print("Again? (y/n): ");
+				cont = input.next().charAt(0);
+			}
+		}
+		catch (Exception ex) {
+			// clear the input buffer
+			input.nextLine();
+			System.out.println("Input Error!");
+		}
+	}
+}
