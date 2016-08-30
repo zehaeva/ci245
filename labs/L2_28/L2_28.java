5278b8cc33cbdb40f9defd3db89387e55852328f
Howard Canaway <howard.canaway@gmail.com>
2016-08-30 17:50:16 -0400

    Lab 2.28
    Code works, it needs some extra formatting

--- labs/L2_28/L2_28.java
+++ labs/L2_28/L2_28.java
@@ -1,7 +1,35 @@
 import java.util.Scanner;
-import java.Math;
+import java.util.InputMismatchException;
 
 public class L2_28 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int radius;
+		char cont = 'y';
+
+		try {
+			while (cont == 'y') {
+		  		System.out.print("Enter the radius of a circle: ");
+				radius 	= input.nextInt();
+
+				System.out.printf("Diameter: %f", 2f * radius);
+				System.out.printf("Circumfrence: %f", Math.PI * radius * 2);
+				System.out.printf("Diameter: %f", Math.PI * radius * radius);
+
+		  		System.out.print("Again? (y/n): ");
+				cont = input.next().charAt(0);
+			}
+		}	
+		catch (InputMismatchException ex) {
+			// clear the input buffer
+			System.out.println("Input Error!");
+		}
+		catch (Exception ex) {
+			System.out.println("Unexpected Error!");
+		}
+		finally {
+			input.nextLine();
+		}		
 	}
 }
