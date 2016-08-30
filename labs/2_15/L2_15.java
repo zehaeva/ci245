c342707b22c173759523c29f45c61bf79eedcd66
Howard Canaway <howard.canaway@gmail.com>
2016-08-30 17:06:54 -0400

    Lab 2.15
    Added error handling and a complete cycle to aid in testing.

--- labs/2_15/L2_15.java
+++ labs/2_15/L2_15.java
@@ -6,22 +6,29 @@ public class L2_15 {
 
 		int first;
 		int second;
-		char not_done = 'n';
+		char not_done = 'y';
 
-		while (not_done == 'n') {
-			System.out.print("Please enter the first integer: ");
-			first = input.nextInt();
+		while (not_done == 'y') {
+			try {
+				System.out.print("Please enter the first integer: ");
+				first = input.nextInt();
 
-			System.out.print("Please enter the second integer: ");
-			second = input.nextInt();
+				System.out.print("Please enter the second integer: ");
+				second = input.nextInt();
 
-			System.out.printf("Sum: %d\n", first + second);
-			System.out.printf("Product: %d\n", first * second);
-			System.out.printf("Difference: %d\n", first - second);
-			System.out.printf("Quotient: %d\n", first / second);
-			
-			System.out.print("Another? (y/n): ");
-		 	not_done = input.next().charAt(0);	
+				System.out.printf("Sum: %d\n", first + second);
+				System.out.printf("Product: %d\n", first * second);
+				System.out.printf("Difference: %d\n", first - second);
+				System.out.printf("Quotient: %d\n", first / second);
+				
+				System.out.print("Another? (y/n): ");
+				not_done = input.next().charAt(0);	
+			}
+			catch (Exception ex) {
+				// this should clear the input buffer
+				input.nextLine();
+				System.out.print("Invalid Input!\n");
+			}
 		}
 	}
 }
