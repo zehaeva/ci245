bc86fd0082246ae9664d69cbd6bc4d60a91671ce
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-01 19:41:04 -0400

    Homework 5.15
    Printing Triangles, first one is done

--- homework/H5_15/H5_15.java
+++ homework/H5_15/H5_15.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class H5_15 {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(Sytem.in);
+		Scanner input = new Scanner(System.in);
+
+		for (int i=1;i<11;i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
 	}
 }
