f01c9635cd51e1d2950620e336ddd8c0adea7220
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-01 19:13:15 -0400

    Homework 2.31
    Initial workings

--- homework/H2_31/H2_31.java
+++ homework/H2_31/H2_31.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 
 public class H2_31 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int max_n;
+
+		System.out.print("Enter the max number to compute: ");
+		max_n = input.nextInt();
+
+
+		System.out.printf("number\tsquare\tcube\n");
+		for (int i=0;i<=max_n;i++) {
+			System.out.printf("%d\t%d\t%d\n", i, i*i, i*i*i);
+		}
 	}
 }
