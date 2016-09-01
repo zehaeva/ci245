c3c90e20ce96e2d543e8600ccbb86e429893f89e
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-01 15:01:30 -0400

    Homework 4.31
    Validation added, along with error handling.

--- homework/H4_31/H4_31.java
+++ homework/H4_31/H4_31.java
@@ -9,18 +9,35 @@ public class H4_31 {
 		int deci = 0;
 		int binarylength = 0;
 		double placemulti;
+		char cont = 'y';
+		String validate = "^[01]+$";
 
-		System.out.print("Enter a number in binary: ");
-		binary = input.nextLine();
+		while (cont == 'y') {
+			try {
+				System.out.print("Enter a number in binary: ");
+				binary = input.nextLine();
 
-		binarylength = binary.length();
+				if (binary.matches(validate)) {
+					binarylength = binary.length();
 
-		for (int i=0;i<binarylength;i++) {
-			placemulti = Math.pow(2, i);
-			deci = Double.valueOf(placemulti).intValue() * Character.getNumericValue(binary.charAt(binarylength - i - 1)) + deci;
-			System.out.println(placemulti);
+					for (int i=0;i<binarylength;i++) {
+						placemulti = Math.pow(2, i);
+						deci = Double.valueOf(placemulti).intValue() * Character.getNumericValue(binary.charAt(binarylength - i - 1)) + deci;
+					}
+
+					System.out.printf("Your binary number, %s, is %d in decimal!\n", binary, deci);
+
+					System.out.print("Another? (y/n): ");
+					cont = input.nextLine().charAt(0);
+				}
+				else {
+					System.out.println("Hey! That number isn't binary! Let's try that again!");
+				}
+			}
+			catch (Exception ex) {
+				input.nextLine();
+				System.out.println("Oops! Something went wrong here, let's try that again!");
+			}
 		}
-
-		System.out.printf("Your binary number, %s, is %d in decimal!\n", binary, deci);
 	}
 }
