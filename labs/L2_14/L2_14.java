da30a2a721ae537c9a27f40408ba99e7281bd9f6
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-01 19:21:02 -0400

    Lab 2.14
    Fixed part of the requirements of the problem, print needed to be four
    separate statments

---
+++ labs/L2_14/L2_14.java
@@ -0,0 +1,10 @@
+class L2_14 {
+	public static void main (String[] args) {
+		System.out.println("1 2 3 4");
+		System.out.printf("%s %s %s %s\n", 1, 2, 3, 4);
+		System.out.print("1 ");
+		System.out.print("2 ");
+		System.out.print("3 ");
+		System.out.print("4\n ");
+	}
+}
