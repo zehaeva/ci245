55fde613c0cd19cd1e6b5424a9e94fc10e9b8ed0
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-01 20:03:25 -0400

    Homework 5.15
    Program works but I can't generalize the last case!

--- homework/H5_15/H5_15.java
+++ homework/H5_15/H5_15.java
@@ -1,12 +1,26 @@
-import java.util.Scanner;
-
 public class H5_15 {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-
-		for (int i=1;i<11;i++) {
+		print_triangle(1, 11, 0, '*', ' ');
+		print_triangle(1, 11, 11, ' ', '*');
+		print_triangle(0, 11, 0, ' ', '*');
+		for (int i=10;i>0;i--) {
 			for(int j=0;j<i;j++) {
+				System.out.print(' ');
+			}
+			for(int j=11;j>i;j--) {
 				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+
+	public static void print_triangle(int a, int b, int c, char e, char f) {
+		for (int i=a;i<b;i++) {
+			for(int j=c;j<i;j++) {
+				System.out.print(e);
+			}
+			for(int j=b;j>i;j--) {
+				System.out.print(f);
 			}
 			System.out.println();
 		}
