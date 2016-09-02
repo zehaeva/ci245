976a37f4f0f8b669442a7e04e677c85d9048bb6b
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-01 20:53:15 -0400

    Lab 7.19
    Airline Reservation System, inital commit

---
+++ labs/L7_19/L7_19.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class L7_19 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		boolean[] seats = {false, false, false, false, false, false, false, false, false, false};
+
+	}
+}
