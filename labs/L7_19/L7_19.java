4e0c072d84f39e178afdb92c1d0de249b7fb2a45
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-03 12:37:05 -0400

    Lab 7.19
    Added some error handling
    stated refactor of the finding seats and jumping to next seating class
    made the seat class to have a function to return the string of the class

--- labs/L7_19/L7_19.java
+++ labs/L7_19/L7_19.java
@@ -22,10 +22,18 @@ public class L7_19 {
 			cont = 'y';
 			found_seat = -1;
 			change = 'n';
+			seat_type = 0;
 
-
-			System.out.print("Please type 1 for First Class or type 2 for Economy: ");
-			seat_type = input.nextInt();
+			while (seat_type == 0) {
+				try {
+					System.out.print("Please type 1 for First Class or type 2 for Economy: ");
+					seat_type = input.nextInt();
+				}
+				catch (Exception ex) {
+					input.nextLine();
+					System.out.println("Sorry I didn't quite understand that!");
+				}
+			}
 
 			while (cont == 'y') {
 				if (seat_type == 1) {
@@ -35,7 +43,7 @@ public class L7_19 {
 							cont = 'n';
 							found_seat = i;
 							seats[i] = true;
-							print_pass(i, "First Class");
+							print_pass(i, seat_type);
 							break;
 						}
 					}
@@ -59,7 +67,7 @@ public class L7_19 {
 							cont = 'n';
 							found_seat = i;
 							seats[i] = true;
-							print_pass(i, "Economy");
+							print_pass(i, seat_type);
 							break;
 						}
 					}
@@ -77,6 +85,7 @@ public class L7_19 {
 					}
 				}
 			}
+			
 
 			if (filled_seats >= 10) {
 				System.out.println("This flight is fully booked! The next flight leaves in 3 horus");
@@ -89,12 +98,56 @@ public class L7_19 {
 		}
 	}
 
-	public static void print_pass(int seat_number, String seat_class) {
+	public static String seat_class(int class_id) {
+		if (class_id == 1) {
+			return "First Class";
+		}
+		else if (class_id == 2) {
+			return "Economy";
+		}
+		else {
+			return "Unknown";
+		}
+	}
+
+	public static void find_seat(int section_start, int section_end, int class_id, boolean[] seating_chart) {
+		int seat_type;
+		Scanner input = new Scanner(System.in);
+		int found_seat = 0;
+		int filled_seats = 0;
+		char change;
+		char cont;
+		char another = 'y';
+
+		for (int i=section_start;i<section_end;i++) {
+			if (seating_chart[i] == false) {
+				filled_seats++;
+				cont = 'n';
+				found_seat = i;
+				seating_chart[i] = true;
+				print_pass(i, class_id);
+				break;
+			}
+		}
+
+		if (found_seat < 0) {
+			System.out.printf("Sorry we didn't find an available seat, is %s acceptable? (y/n): ", seat_class(2));
+			change = input.next().charAt(0);
+			if (change == 'y') {
+				seat_type = 1;
+			}
+			else {
+				System.out.println("Next flight leaves in 3 hours");
+			}
+		}
+	}
+
+	public static void print_pass(int seat_number, int class_id) {
 		System.out.printf("Your seat has been reserved! Your seat number is %d\n", seat_number);
 
 		System.out.printf("****************************************************\n");
 		System.out.printf("** Boarding Pass                                  **\n");
-		System.out.printf("** Seat# %-26d %-13s **\n", seat_number, seat_class);
+		System.out.printf("** Seat# %-26d %-13s **\n", seat_number, seat_class(class_id));
 		System.out.printf("**                                                **\n");
 		System.out.printf("****************************************************\n");
 	}
