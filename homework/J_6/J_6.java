2dee6628c197d7d62ccaee63a024e1caa91232cc
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-06 14:19:29 -0400

    Lab J.6
    Implimented look up for the matrix, doesn't handle face cards yet

--- homework/J_6/J_6.java
+++ homework/J_6/J_6.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Hashtable;
 
 /**
 * This class will take in three cards as input and will output the
@@ -37,6 +38,7 @@ public class J_6 {
 		// Let's find out what we are supposed to do!
 
 		// ************ WRITE YOUR CODE HERE ******************* //
+		output = player_action(pCard1Value, pCard2Value, dCardValue);
 
 		// OK, we're all done now we should know our move and can print it!
 
@@ -63,6 +65,7 @@ public class J_6 {
 	}
 
 	public static String player_action(int card1, int card2, int dealer) {
+		int d_index = dealer - 2;
 	//	encode matrix
 		String[][] actions = {{"H","H","H","H","H","H","H","H","H","H",},
 							  {"H","D","D","D","D","H","H","H","H","H",},
@@ -81,8 +84,8 @@ public class J_6 {
 							  {"H","H","D","D","D","H","H","H","H","H",},
 							  {"H","H","H","D","D","H","H","H","H","H",},
 							  {"H","H","H","D","D","H","H","H","H","H",},
-							  {"H","H","H","H","H","H","H","H","H","H",},
-							  {"H","H","H","H","H","H","H","H","H","H",},
+							  {"P","P","P","P","P","P","P","P","P","P",},
+							  {"P","P","P","P","P","P","P","P","P","P",},
 							  {"S","S","S","S","S","S","S","S","S","S",},
 							  {"P","P","P","P","P","S","P","P","S","S",},
 							  {"P","P","P","P","P","P","H","H","H","H",},
@@ -92,6 +95,76 @@ public class J_6 {
 							  {"H","H","P","P","P","P","H","H","H","H",},
 							  {"H","H","P","P","P","P","H","H","H","H",},
 						     };
+		if (card1 == card2) {
+			switch(card1) {
+				case 2: return actions[26][d_index];
+				case 3: return actions[25][d_index];
+				case 4: return actions[24][d_index];
+				case 5: return actions[23][d_index];
+				case 6: return actions[22][d_index];
+				case 7: return actions[21][d_index];
+				case 8: return actions[20][d_index];
+				case 9: return actions[19][d_index];
+				case 10: return actions[18][d_index];
+				case 11: return actions[17][d_index];
+			}
+		}
+		else if (card1 == 11 || card2 == 11) {
+			if (card1 == 2 || card2 == 2) {
+				return actions[16][d_index];
+			}
+			else if (card1 == 3 || card2 == 3) {
+				return actions[15][d_index];
+			}
+			else if (card1 == 4 || card2 == 4) {
+				return actions[14][d_index];
+			}
+			else if (card1 == 5 || card2 == 5) {
+				return actions[13][d_index];
+			}
+			else if (card1 == 6 || card2 == 6) {
+				return actions[12][d_index];
+			}
+			else if (card1 == 7 || card2 == 7) {
+				return actions[11][d_index];
+			}
+		    else if (card1 >= 8 || card2 >= 8) {
+				return actions[10][d_index];
+			}
+		}
+		else {
+			if (card1 + card2 <= 8) {
+				return actions[0][d_index];
+			}
+			else if (card1 + card2 >= 17) {
+				return actions[9][d_index];
+			}
+			else if (card1 + card2 == 16) {
+				return actions[8][d_index];
+			}
+			else if (card1 + card2 == 15) {
+				return actions[7][d_index];
+			}
+			else if (card1 + card2 == 14) {
+				return actions[6][d_index];
+			}
+			else if (card1 + card2 == 13) {
+				return actions[5][d_index];
+			}
+			else if (card1 + card2 == 12) {
+				return actions[4][d_index];
+			}
+			else if (card1 + card2 == 11) {
+				return actions[3][d_index];
+			}
+			else if (card1 + card2 == 10) {
+				return actions[2][d_index];
+			}
+			else if (card1 + card2 == 9) {
+				return actions[1][d_index];
+			}
+		}
+
 		return "H";
 	}
 }
