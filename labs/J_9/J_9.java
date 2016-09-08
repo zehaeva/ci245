7e672d88cddd63cc5c412d141acb1ef354466a47
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-08 18:32:34 -0400

    Lab J.9
    implemented both versions of mid
    
    I noticed in the documention that mid is zero based,
    I should add that into this program

--- labs/J_9/J_9.java
+++ labs/J_9/J_9.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
  */
 public class J_9 {
 	public static void main(String[] args) {
+		String str = "hello world!";
+		System.out.printf("%s\n", left(str, 4));
+		System.out.printf("%s\n", right(str, 5));
+		System.out.printf("%s\n", mid(str, 4));
+		System.out.printf("%s\n", mid(str, 3, 4));
 	}
 
 	/**
@@ -23,16 +28,26 @@ public class J_9 {
 	 * @param length number of characters to return
 	 */
 	public static String right(String str, int length) {
-		return str.substring((str.length() - length), length);
+		return str.substring((str.length() - length), str.length());
 	}
 	/**
 	 * Returns a string that contains all the characters starting from a specified position in a string.
 	 * @return string of specified length 
 	 * @param str String to parse
-	 * @param start number of characters to return
+	 * @param start index of the start of the string to return
 	 */
 	public static String mid(String str, int start) {
-		return str.substring(start);
+		return str.substring(start, str.length());
+	}
+	/**
+	 * Returns a string that contains all the characters starting from a specified position in a string.
+	 * @return string of specified length 
+	 * @param str String to parse
+	 * @param start index of the start of the string to return
+	 * @param length number of characters to return
+	 */
+	public static String mid(String str, int start, int length) {
+		return str.substring(start, length + start);
 	}
 	
 }
