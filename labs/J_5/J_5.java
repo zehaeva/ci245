8bc3ffad3b991f9dfb1c7f45aa75001267372878
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-08 08:57:39 -0400

    Lab J.5
    As extra credit I changed this so that it computes the determinate
    for any sized matrix

--- labs/J_5/J_5.java
+++ labs/J_5/J_5.java
@@ -1,11 +1,13 @@
 public class J_5 {
 	public static void main(String[] args) {
 		double[][] matrix = {{1,4,7}, {3,0,5}, {-1,9,11}};
+		//double[][] matrix = {{3,0,2,-1}, {1,2,0,-2}, {4,0,6,-3}, {5,0,2,0}};
 
 		for (int i=0;i<matrix.length;i++) {
 			for (int j=0;j<matrix.length;j++) {
-				System.out.printf("%f\n", minor(matrix, i, j));
+				System.out.printf("%f\t", minor(matrix, i, j));
 			}
+			System.out.println();
 		}
 	}
 
@@ -35,6 +37,30 @@ public class J_5 {
 	}
 
 	public static double determinant(double[][] matrix) {
-		return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];		
+		double det = 0;
+		if (matrix.length == 2) {
+			det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+		}
+		else {
+			double[][] temp = new double[matrix.length - 1][matrix.length - 1];
+			int skip = 0;
+			double sign = 1.0;
+			for (int k=0;k<matrix.length;k++) {
+				for (int i=0;i<matrix.length-1;i++) {
+					skip = 0;
+					for (int j=0;j<matrix.length-1;j++) {
+						if (j == k) {
+							skip = 1;
+						}
+						temp[i][j] = matrix[i + 1][j + skip];
+					}
+				}	
+
+				det += sign * matrix[0][k] * (determinant(temp));
+				sign *= -1.0;
+				skip = 0;
+			}
+		}
+		return det;
 	}
 }
