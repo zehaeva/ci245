051a2258ba224ec916ca98d3d4666e094969cd7c
zehaeva@gmail.com <zehaeva@gmail.com>
2016-09-15 16:18:56 -0400

    14.27 Spam Scoring
    Inital commit, I'm mostly done, I just need a list of 30 words/phrases
    for the filer

---
+++ homework/H4_27/H4_27.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class H4_27 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		char cont = 'y';
+		String email;
+		String[] spamwords = {"hello", "how", "are", "john"};
+		double score = 0.0;
+
+	//	get input
+		System.out.print("Please enter the email to be scored: ");
+		email = input.nextLine();
+
+	//	score the spam
+		score = score_spam(spamwords, email);
+
+	//	tell the user about it
+		output_result(score);
+	}
+
+	public static void output_result(double score) {
+		System.out.printf("This email was scored %f%% spam!\n", score * 100);
+	}
+
+	public static double score_spam(String[] spam_list, String email) {
+		int count = 0;
+		int last_index;
+
+		email = email.toLowerCase();
+
+		for (String spam: spam_list) {
+			last_index = 0;
+			while(last_index >= 0) {
+				last_index = email.indexOf(spam, last_index);
+				if (last_index >= 0) {
+					count++;
+					last_index++;
+				}
+			}
+		}
+
+		return (((double)count) / ((double)words.length));
+	}
+}
