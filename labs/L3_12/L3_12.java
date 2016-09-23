4d520b7c5795140656b194ae003f398a0fe73196
Howard Canaway <zehaeva@gmail.com>
2016-09-22 20:52:07 -0400

    Lab 4) 3.12) Invoices
    More test instances

--- labs/L3_12/L3_12.java
+++ labs/L3_12/L3_12.java
@@ -1,6 +1,20 @@
 public class L3_12 {
     public static void main(String[] args) {
-        Invoice inv = new Invoice("temp", "a temp Object", 1, 50.0);
+        Invoice inv = new Invoice("temp", "a temp Object", 20, 50.0);
+        System.out.println(inv.toString());
 
+        System.out.printf("Invoice Total: %f\n", inv.getInvoiceAmount());
+
+        inv.setQuantity(-5);
+        System.out.println(inv.toString());
+
+        inv.setPartPrice(-4);
+        System.out.println(inv.toString());
+
+        inv.setPartPrice(4);
+        System.out.println(inv.toString());
+
+        inv.setPartNumber("Steel Billet");
+        System.out.println(inv.toString());
     }
 }
