2498b0df47bdc0d39f7fda83f506b0b68674f128
Howard Canaway <zehaeva@gmail.com>
2016-09-25 14:14:36 -0400

    Lab 4) J.17 Sets
    print_tree and print_tree_reverse work well
    print_hash needs to sort
    print_hash_reverse tbd

--- labs/J_17/J_17.java
+++ labs/J_17/J_17.java
@@ -8,16 +8,50 @@ import java.util.TreeSet;
 public class J_17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String hold;
+        String source_text;
+        String[] source_array;
         HashSet<String> hash = new HashSet<String>();
         TreeSet<String> tree = new TreeSet<String>();
 
         //get input
+        System.out.print("Enter a line of text: ");
+        source_text = input.nextLine();
+        source_array = source_text.split(" ");
+        //store in hash/tree set
+        for (String word :
+                source_array) {
+            hash.add(word);
+            tree.add(word);
+        }
 
-        //store in hashset
-        //sort hashset
-        //print hashset
-        //reverse sort
-        //print hashset
+        System.out.println("Hash: ");
+        print_hash(hash);
+        System.out.println("Tree: ");
+        print_tree(tree);
+        print_tree_reverse(tree);
+    }
+
+    private static void print_tree_reverse(TreeSet<String> tree) {
+        for (String word:
+             tree.descendingSet()) {
+            System.out.printf("%s ", word);
+        }
+        System.out.println();
+    }
+
+    private static void print_tree(TreeSet<String> tree) {
+        for (String word :
+                tree) {
+            System.out.printf("%s ", word);
+        }
+        System.out.println();
+    }
+
+    private static void print_hash(HashSet<String> hash) {
+        for (String word :
+                hash) {
+            System.out.printf("%s ", word);
+        }
+        System.out.println();
     }
 }
