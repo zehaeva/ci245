1a5721a191df48ed3e7aae37dfe9abded4cc1715
Howard Canaway <zehaeva@gmail.com>
2016-09-25 12:46:54 -0400

    Lab 4) J.17) HashSet & TreeSet sorting
    Initial Commit, outlined code in comments

---
+++ labs/J_17/J_17.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+/**
+ * Created by zehaeva on 9/25/16.
+ */
+public class J_17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String hold;
+        HashSet<String> hash = new HashSet<String>();
+        TreeSet<String> tree = new TreeSet<String>();
+
+        //get input
+
+        //store in hashset
+        //sort hashset
+        //print hashset
+        //reverse sort
+        //print hashset
+    }
+}
