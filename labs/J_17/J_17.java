f3ca6c56c9a0722c9f6d41d1ad18039a43b8757d
Howard Canaway <zehaeva@gmail.com>
2016-09-25 14:23:57 -0400

    Lab 4) J.17 HashSet and TreeSet
    Implemented print_hash and print_hash_reverse, uses a treeset to sort the hashset.

--- labs/J_17/J_17.java
+++ labs/J_17/J_17.java
@@ -6,6 +6,11 @@ import java.util.TreeSet;
  * Created by zehaeva on 9/25/16.
  */
 public class J_17 {
+    /**
+     * accepts a string of text and then prints out the unique words
+     * utilizing a treeset and a hashset
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String source_text;
@@ -26,11 +31,17 @@ public class J_17 {
 
         System.out.println("Hash: ");
         print_hash(hash);
+        print_hash_reverse(hash);
+
         System.out.println("Tree: ");
         print_tree(tree);
         print_tree_reverse(tree);
     }
 
+    /**
+     * prints a treeset in reverse order
+     * @param tree
+     */
     private static void print_tree_reverse(TreeSet<String> tree) {
         for (String word:
              tree.descendingSet()) {
@@ -39,6 +50,10 @@ public class J_17 {
         System.out.println();
     }
 
+    /**
+     * prints a treeset
+     * @param tree
+     */
     private static void print_tree(TreeSet<String> tree) {
         for (String word :
                 tree) {
@@ -47,9 +62,29 @@ public class J_17 {
         System.out.println();
     }
 
+    /**
+     * prints a hashset in order, utilizing a treeset to sort the hashset
+     * @param hash
+     */
     private static void print_hash(HashSet<String> hash) {
+        TreeSet<String> tree = new TreeSet<String>();
+        tree.addAll(hash);
         for (String word :
-                hash) {
+                tree) {
+            System.out.printf("%s ", word);
+        }
+        System.out.println();
+    }
+
+    /**
+     * prints a hashset in reverse order utilizing a treeset
+     * @param hash
+     */
+    private static void print_hash_reverse(HashSet<String> hash) {
+        TreeSet<String> tree = new TreeSet<String>();
+        tree.addAll(hash);
+        for (String word :
+                tree.descendingSet()) {
             System.out.printf("%s ", word);
         }
         System.out.println();
