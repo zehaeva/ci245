c2c022cfeaef5bf6e74320a7e4e8aee80d23564f
Howard Canaway <zehaeva@users.noreply.github.com>
2016-09-29 17:03:44 -0400

    Update H18_15.java
    a lot of progress made here, System functions perfectly fine, I need to code in the validation for diagonals and it should work!

--- homework/H18_15/H18_15.java
+++ homework/H18_15/H18_15.java
@@ -3,6 +3,11 @@ public class H18_15 {
 	// write your code here
         boolean[][] board = new boolean[8][8];
 
+        for (boolean[] col :
+                board) {
+            Arrays.fill(col, false);
+        }
+
         board = place_queen(board, 0, 0);
 
         print_board(board);
@@ -20,9 +25,112 @@ public class H18_15 {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     public static boolean[][] place_queen(boolean[][] board, int row, int col) {
-        return board;
+        boolean[][] temp;
+
+        board[row][col] = true;
+
+        if (finished(board)) {
+            return board;
+        }
+        else if (!valid(board)) {
+            return new boolean[1][1];
+        }
+        else {
+            while (row < 8 && col < 8) {
+                // System.out.printf("Count: %d, row: %d, col: %d\n", queen_count(board), row, col);
+                if (row == 7) {
+                    row = 0;
+                    col++;
+                } else {
+                    row++;
+                }
+
+                temp = copy_board(board);
+
+                temp = place_queen(temp, row, col); //copy_board(board);
+
+                //  place a queen there
+                if (valid(temp)) {
+                    return temp;
+                }
+            }
+            return new boolean[1][1];
+        }
+    }
+
+    private static boolean[][] copy_board(boolean[][] board) {
+        boolean[][] temp = new boolean[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                temp[i][j] = board[i][j];
+            }
+        }
+
+        return temp;
+    }
+
+    private static boolean valid(boolean[][] board) {
+        boolean am_i_valid = true;
+
+        if (board.length == 8) {
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (board[i][j]) {
+                        for (int k = 0; k < 8; k++) {
+                            //  check row
+                            if (i != k && board[i][k]) {
+                                am_i_valid = false;
+                            }
+                            //  check col
+                            if (j != k && board[k][j]) {
+                                am_i_valid = false;
+                            }
+
+                            if (!am_i_valid) {
+                                break;
+                            }
+                        }
+                    }
+                    if (!am_i_valid) {
+                        break;
+                    }
+                }
+                if (!am_i_valid) {
+                    break;
+                }
+            }
+        }
+        else {
+            am_i_valid =  false;
+        }
+        return am_i_valid;
+    }
+
+    public static int queen_count(boolean[][] board) {
+        int count = 0;
+
+        for (int i = 0; i <8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j]) {
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static boolean finished(boolean[][] board) {
+        if (queen_count(board) == 8) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
