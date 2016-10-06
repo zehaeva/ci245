aa4263cd66f58271b9eeb860ce43afa3652321da
Howard Canaway <hcanaway@oriskanymfg.com>
2016-10-06 14:46:44 -0400

    Lab 6: 12.8: GUIs
    Form layout complete

---
+++ labs/L12_8/L12_8.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by hcanaway on 10/6/2016.
+ */
+public class L12_8 {
+    private JCheckBox checkBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+
+}
