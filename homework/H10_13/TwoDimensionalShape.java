aff3f1205ba1e083bbc2ef0ff9c19d84c88fd71e
Howard Canaway <zehaeva@users.noreply.github.com>
2016-10-05 21:42:28 -0400

    Create TwoDimensionalShape.java

---
+++ homework/H10_13/TwoDimensionalShape.java
@@ -0,0 +1,11 @@
+/**
+ * 2d Shape info!
+ **/
+public class TwoDimensionalShape {
+  private int _height;
+  private int _width;
+  
+  public int getArea() {
+    return this._height * this_width;
+  }
+}
