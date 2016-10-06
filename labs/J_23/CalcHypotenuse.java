78cd23e55a4fb90c0406b6ebdc6dfab7a790d52a
Howard Canaway <hcanaway@oriskanymfg.com>
2016-10-06 13:26:33 -0400

    Lab 6: J.23: GUIs
     Form is created and laid out

---
+++ labs/J_23/CalcHypotenuse.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by hcanaway on 10/6/2016.
+ */
+public class CalcHypotenuse {
+    private JTextField sideOne;
+    private JPanel panel1;
+    private JTextField sideTwo;
+    private JButton calculateButton;
+    private JTextField hypotenuse;
+}
---
+++ labs/J_23/readme.md
@@ -0,0 +1,6 @@
+# J.23: 
+Create a window that calculates the hypotenuse from two sides of a triangle.  
+
+It should work as such: enter the 2 sides in text fields and then press a calculate button.  
+
+The result is then displayed in the same window on a label or textfield.
