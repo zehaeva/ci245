24abf7108cac7667f1ba0d0ea3232b5c3081e6d1
Howard Canaway <hcanaway@oriskanymfg.com>
2016-10-06 14:22:41 -0400

    Lab 6: J.23: GUIs
    Form now loads and displays

--- labs/J_23/CalcHypotenuse.java
+++ labs/J_23/CalcHypotenuse.java
@@ -1,12 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by hcanaway on 10/6/2016.
  */
-public class CalcHypotenuse {
+public class CalcHypotenuse extends JFrame {
     private JTextField sideOne;
     private JPanel panel1;
     private JTextField sideTwo;
     private JButton calculateButton;
     private JTextField hypotenuse;
+
+    public CalcHypotenuse() {
+        super("Calc Hypotenuse");
+        setContentPane(panel1);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!sideOne.getText().isEmpty() && !sideTwo.getText().isEmpty()) {
+                    hypotenuse.setText("SOMETHING!");
+                }
+            }
+        });
+
+        setVisible(true);
+    }
 }
---
+++ labs/J_23/J_23.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hcanaway on 10/6/2016.
+ */
+public class J_23 {
+    public static void main(String[] args) {
+        CalcHypotenuse form = new CalcHypotenuse();
+
+        form.setVisible(true);
+    }
+}
