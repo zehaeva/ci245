d9d0a3690caa2dcd79c48444bfaf46f3f6d0e52e
Howard Canaway <zehaeva@gmail.com>
2016-10-06 20:29:25 -0400

    Lab 5: J.23
    Changed so that the program doesn't utilize the .form

--- labs/J_23/CalcHypotenuse.java
+++ labs/J_23/CalcHypotenuse.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,6 +15,7 @@ public class CalcHypotenuse extends JFrame {
 
     public CalcHypotenuse() {
         super("Calc Hypotenuse");
+        populatePanel();
         setContentPane(panel1);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,5 +30,37 @@ public class CalcHypotenuse extends JFrame {
         });
 
         setVisible(true);
+    }
+
+    private void populatePanel() {
+        this.panel1 = new JPanel();
+        this.panel1.setLayout(new GridLayout(4, 2));
+
+        JLabel label = new JLabel();
+        label.setText("Side One:");
+        this.panel1.add(label);
+
+        this.sideOne = new JTextField();
+        this.panel1.add(this.sideOne);
+
+        JLabel label1 = new JLabel();
+        label1.setText("Side Two:");
+        this.panel1.add(label1);
+
+        this.sideTwo = new JTextField();
+        this.panel1.add(this.sideTwo);
+
+        this.panel1.add(new JLabel());
+
+        this.calculateButton = new JButton();
+        this.calculateButton.setText("Calculate");
+        this.panel1.add(this.calculateButton);
+
+        this.panel1.add(new JLabel());
+
+        this.hypotenuse = new JTextField();
+        this.panel1.add(this.hypotenuse);
+
+        add(this.panel1);
     }
 }
