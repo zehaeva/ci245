22440e50dabd15037410b3982bc4fe5df78d1ff9
Howard Canaway <hcanaway@oriskanymfg.com>
2016-10-11 15:12:28 -0400

    Project: Angry Cousins
    Moved some files around
    added file reading code
    added data validation code, it's not tested.

--- projects/RunSeatingChart/SeatingChart.java
+++ projects/RunSeatingChart/SeatingChart.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 /**
  * This class determines the seating chart of a wedding such that no two people
  * who hate each other are sitting at the same table.
@@ -24,7 +31,7 @@ public class SeatingChart {
 	public String[] getStudentNames() {
 		// return new String[] { "One Person Working Alone" };
 		// return new String[] { "First Partner", "Second Partner" };
-		return new String[] {};
+		return new String[] {"Howard Canaway"};
 	}
 
 	/**
@@ -39,9 +46,69 @@ public class SeatingChart {
 	 *         cannot be read or opened, and any other number otherwise.
 	 */
 	public int ValidateInputFile(String filename) {
+		int code = 0;
+		String[] temp;
 
-		return 0;
+		try {
+			ArrayList<String> file_to_verify = ReadTextFile(filename);
+
+			if (!file_to_verify.get(0).matches("^[0-9]+$")) {
+				code = 1;
+			}
+			else if (Integer.parseInt(file_to_verify.get(0)) < 10 || Integer.parseInt(file_to_verify.get(0)) > 200) {
+				code = 1;
+			}
+			else if (!file_to_verify.get(1).equals("5") &&
+					 !file_to_verify.get(1).equals("6") &&
+					 !file_to_verify.get(1).equals("7") &&
+					 !file_to_verify.get(1).equals("8") &&
+					 !file_to_verify.get(1).equals("100000")) {
+				code = 2;
+			} else {
+				for (int i = 2; i < file_to_verify.size() - 1; i++) {
+					if (!file_to_verify.get(i).matches("^[0-9]+ [0-9]+$")) {
+						code = 3;
+						break;
+					}
+					else {
+						temp = file_to_verify.get(i).split(" ");
+						if ((Integer.parseInt(temp[0]) < 1 || Integer.parseInt(temp[0]) > 200) ||
+								(Integer.parseInt(temp[1]) < 1 || Integer.parseInt(temp[1]) > 200) ) {
+							code = 3;
+							break;
+						}
+					}
+				}
+			}
+
+		}
+		catch (IOException e) {
+			code = 4;
+		}
+
+		return code;
 	}// end ValidateInputFile
+
+	/**
+	 * Code stolen from the instructor Coveny J_11
+	 * @param fileName the name of the file to read
+	 * @return arraylist of the file
+	 */
+	private ArrayList<String> ReadTextFile(String fileName) throws IOException {
+		// Now we will read all of the lines back that we just wrote
+		Path path = Paths.get(fileName);
+		ArrayList<String> linesToRead = new ArrayList<String>();
+
+		try {
+			// Need to cast the return result to an ArrayList
+			linesToRead = (ArrayList<String>) Files.readAllLines(path, StandardCharsets.UTF_8);
+
+		} catch (IOException e) {
+			throw e;
+		}
+
+		return linesToRead;
+	}
 
 	/**
 	 * This method should determine the seating chart for the wedding as
