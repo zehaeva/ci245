97f3eb24615dc2ba41cfa6cf40842394a106a0af
Howard Canaway <howard.canaway@gmail.com>
2016-10-18 23:48:40 -0400

    Projects: Midterm
    making headway, I now have a sorted list of who to seat first, from most hated to least hated.

--- projects/RunSeatingChart/SeatingChart.java
+++ projects/RunSeatingChart/SeatingChart.java
@@ -86,8 +86,8 @@ public class SeatingChart {
 							code = 3;
                             break;
 						}
-						else if ((Integer.parseInt(temp[0]) < 1 || Integer.parseInt(temp[0]) > max_guests) ||
-								(Integer.parseInt(temp[1]) < 1 || Integer.parseInt(temp[1]) > max_guests) ) {
+						else if ((Integer.parseInt(temp[0]) < 0 || Integer.parseInt(temp[0]) > max_guests) ||
+								(Integer.parseInt(temp[1]) < 0 || Integer.parseInt(temp[1]) > max_guests) ) {
 							code = 3;
                             System.out.println(i);
 							break;
@@ -112,7 +112,8 @@ public class SeatingChart {
 	 */
 	private ArrayList<String> ReadTextFile(String fileName) throws IOException {
 		// Now we will read all of the lines back that we just wrote
-		Path path = Paths.get("/home/zehaeva/IdeaProjects/ci245/projects/RunSeatingChart/" + fileName);
+		//Path path = Paths.get("/home/zehaeva/IdeaProjects/ci245/projects/RunSeatingChart/" + fileName);
+		Path path = Paths.get("c:\\users\\zehaeva\\IdeaProjects\\ci245\\projects\\RunSeatingChart\\" + fileName);
 		ArrayList<String> linesToRead = new ArrayList<String>();
 
 		try {
@@ -150,39 +151,123 @@ public class SeatingChart {
         this._num_guests = Integer.parseInt(file.get(0));
         this._table_size = Integer.parseInt(file.get(1));
 		this._hate_sort = new int[this._num_guests];
+		this._tables = new int[this._num_guests];
+
+		Arrays.fill(this._tables, 0);
 
         fill_hate_list(file);
 
 		sort_guests();
 
-		//seat_guest(this._hate_sort);
+		this._tables = seat_guest(this._tables);
 
-        for (int i = 0; i < this._num_guests; i++) {
-            System.out.println(this._hate_sort[i]);
-        }
+		print_sort();
 
         return new GuestTable[] { };
 	}
 
+	private int[] seat_guest(int[] assigned_seats) {
+		if (everyone_seated(assigned_seats)) {
+			return assigned_seats;
+		} else {
+			int i = get_next_guest(assigned_seats);
+
+		}
+	}
+
+	private int get_next_guest(int[] assigned_seats) {
+		int temp = 0;
+		for (int i = 0; i < assigned_seats.length; i++) {
+			if (assigned_seats[i] == 0) {
+				temp = i;
+			}
+		}
+		return temp;
+	}
+
+
+	private boolean everyone_seated(int[] assigned_seats) {
+		boolean done = true;
+		for (int val :
+				assigned_seats) {
+			if (val == 0) {
+				done = false;
+				break;
+			}
+		}
+
+		return done;
+	}
+
+	private boolean table_full(int table_num) {
+		int count = 0;
+	//	count the people at table
+		for (int val :
+				this._tables) {
+			if (val == table_num) {
+				count++;
+				if (count == this._table_size) {
+					break;
+				}
+			}
+		}
+		if (count >= this._table_size) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
     private void sort_guests() {
-        int temp;
+        int temp_i;
+		int temp_j;
     //  seed the sort
         for (int i = 0; i < this._num_guests; i++) {
-            this._hate_sort[i] = i + 1;
+            this._hate_sort[i] = i;
         }
+		print_sort();
+
     //  now we iterate over the sort
     //  and find if this person hates less than someone else
     //  then we swap
         for (int i = 0; i < this._num_guests; i++) {
             for (int j = 0; j < this._num_guests; j++) {
-                if (this._hate_radix[this._hate_sort[i]] < this._hate_radix[j]) {
-                    temp = this._hate_sort[i];
-                    this._hate_sort[i] = j;
-                    this._hate_sort[j] = temp;
-                }
+                if (this._hate_radix[i] < this._hate_radix[j]) {
+					temp_i = getSortGuest(i);
+					temp_j = getSortGuest(j);
+					if (temp_i < temp_j) {
+						this._hate_sort[temp_i] = j;
+						this._hate_sort[temp_j] = i;
+					}
+				}
             }
+			//print_sort();
         }
     }
+
+	private int getSortGuest(int i) {
+		int temp_i = 0;
+		for (int k = 0; k < this._num_guests; k++) {
+            if (this._hate_sort[k] == i) {
+                temp_i = k;
+                break;
+            }
+        }
+		return temp_i;
+	}
+
+	private void print_sort() {
+		System.out.println("Hate Sort");
+		for (int i = 0; i < this._num_guests; i++) {
+			System.out.printf("%d: %d\n", i, this._hate_sort[i]);
+		}
+	}
+	private void print_hate() {
+		System.out.println("Hate Radix");
+		for (int i = 0; i < this._num_guests; i++) {
+			System.out.printf("%d: %d\n", i, this._hate_radix[i]);
+		}
+	}
 
     /**
      * fills the list full of hatred for one's kin
@@ -212,6 +297,7 @@ public class SeatingChart {
             this._hate_radix[hate_one]++;
             this._hate_radix[hate_two]++;
         }
+		print_hate();
     }
 
     private void sort_hate() {
