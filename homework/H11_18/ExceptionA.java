7460ae152888de5613e4102ab27591afbde25bc4
Howard Canaway <zehaeva@gmail.com>
2016-10-30 12:00:58 -0400

    Homework: 11.18: Contrived Exceptions
    So this one doesn't compile, because part of the instructions is to write a program that the compiler complains about!

---
+++ homework/H11_18/ExceptionA.java
@@ -0,0 +1,5 @@
+/**
+ * Created by zehaeva on 10/30/16.
+ */
+public class ExceptionA extends Exception {
+}
---
+++ homework/H11_18/H11_18.java
@@ -0,0 +1,16 @@
+/**
+ * Created by zehaeva on 10/27/16.
+ */
+public class H11_18 {
+    public static void main (String[] args) {
+        try {
+            throw new ExceptionB();
+        }
+        catch (ExceptionA e) {
+            System.out.println("Super Class exception!");
+        }
+        catch (ExceptionB e) {
+            System.out.println("We'll never get here so the compiler hates us!");
+        }
+    }
+}
