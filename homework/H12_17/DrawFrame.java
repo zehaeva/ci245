03cb0293f90d7b3f56ea323c71a5882dc00c5f5e
Howard Canaway <hcanaway@oriskanymfg.com>
2016-10-31 18:02:04 -0400

    12.17
    Added Draw Frame, creates a DrawPanel and a StatusLabel and adds them to the frame!

---
+++ homework/H12_17/DrawFrame.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by hcanaway on 10/31/2016.
+ */
+public class DrawFrame extends JFrame {
+    public DrawFrame() throws HeadlessException {
+        this.setLayout(new BorderLayout());
+        JLabel status = new JLabel();
+        DrawPanel dp = new DrawPanel(status);
+        this.add(dp, BorderLayout.CENTER);
+        this.add(status, BorderLayout.SOUTH);
+
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setBackground(Color.white);
+        this.setLocationRelativeTo(null);
+        this.setSize(300, 300);
+    }
+}
