cb5233cf026d1bfaaba85c4409a0e4de5c3a098a
Howard Canaway <hcanaway@oriskanymfg.com>
2016-11-17 16:20:37 -0500

    J.44: Affine Cipher
    created basic encryption function, untested

--- labs/J_44/J_44.java
+++ labs/J_44/J_44.java
@@ -2,4 +2,15 @@
  * Created by hcanaway on 11/17/2016.
  */
 public class J_44 {
+
+    public static String encrypt(String text, int multiplier, int shift) {
+        String return_me = "";
+        for (char a :
+                text.toLowerCase().toCharArray()) {
+            int value = a - 65;
+            int new_value = ((value * multiplier + shift ) % 26);
+            return_me += new_value;
+        }
+        return return_me;
+    }
 }
