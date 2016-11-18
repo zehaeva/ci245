7c97f9d90e4ad976969c65ed5507b777e6e0d626
Howard Canaway <hcanaway@oriskanymfg.com>
2016-11-18 08:46:36 -0500

    J.44: Affline Cypher
    Implemented better algo for finding the modulo inverse and then made the decrypt string a better function as well.

--- labs/J_44/J_44.java
+++ labs/J_44/J_44.java
@@ -2,6 +2,10 @@
  * Created by hcanaway on 11/17/2016.
  */
 public class J_44 {
+    /**
+     * takes the given string and then encrypts it and decrypts it
+     * @param args
+     */
     public static void main(String[] args) {
         int mult = 5;
         int shift = 4;
@@ -10,6 +14,28 @@ public class J_44 {
         System.out.println(decrypt(temp, mult, shift));
     }
 
+    /**
+     * Finds the Modulo Inverse for 26 and the multiplier
+     * @param mult
+     * @return
+     */
+    public static int mod_inverse(int mult) {
+        int i;
+        for (i=0;i<26;i++) {
+            if ((mult * i) % 26 == 1) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * Encrypts the string using the Affline Cypher
+     * @param text
+     * @param multiplier
+     * @param shift
+     * @return
+     */
     public static String encrypt(String text, int multiplier, int shift) {
         String return_me = "";
         for (char a :
@@ -21,24 +47,23 @@ public class J_44 {
         return return_me;
     }
 
+    /**
+     * Decrypts the string using the given info
+     * @param text
+     * @param multiplier
+     * @param shift
+     * @return
+     */
     public static String decrypt(String text, int multiplier, int shift) {
         String return_me = "";
+        int mi = mod_inverse(multiplier);
         for (char a :
                 text.toLowerCase().toCharArray()) {
             int value = a - 97;
-            int new_value = mod_inverse(value, multiplier, shift);
+            int new_value = (mi * (value - shift)) % 26;
             return_me += (char)(new_value + 97);
         }
         return return_me;
     }
 
-    public static int mod_inverse(int value, int mult, int shift) {
-        int i;
-        for (i=0;i<26;i++) {
-            if (((26*i + value - shift) % mult) == 0){
-                break;
-            }
-        }
-        return ((26 * i + value - shift) / mult);
-    }
 }
