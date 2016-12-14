ebef0a2e37e4ae51fc459e7613a38ddc17ae3f84
Howard Canaway <zehaeva@gmail.com>
2016-12-14 18:29:55 -0500

    Final Project
    Compacted Bishops movement generation

--- projects/FinalProject/Bishop.java
+++ projects/FinalProject/Bishop.java
@@ -12,10 +12,12 @@ public class Bishop extends Unit {
     public Bishop(int x, int y, int size, int speedX, int speedY, Color color, Point facing) {
         super(x, y, size, speedX, speedY, color, facing);this._hit_points = 5;
 
-        this._possible_moves.add(new Point(1, 1));
-        this._possible_moves.add(new Point(1, -1));
-        this._possible_moves.add(new Point(-1, -1));
-        this._possible_moves.add(new Point(-1, 1));
+        for (int i = 1; i < 4; i++) {
+            this._possible_moves.add(new Point(i, i));
+            this._possible_moves.add(new Point(i, -i));
+            this._possible_moves.add(new Point(-i, -i));
+            this._possible_moves.add(new Point(-i, i));
+        }
 
         this._attack_patterns.add(new Point(0, 1));
         this._attack_patterns.add(new Point(0, -1));
